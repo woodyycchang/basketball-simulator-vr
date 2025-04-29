117a9f09def4f94b15b3618f30fae94bf6dc367f
Zixuan Peng <zpeng94@wisc.edu>
2025-04-28 21:36:57 -0500

    Added Scoreboard UI, showing score

--- Assets/BasketScore.cs
+++ Assets/BasketScore.cs
@@ -1,15 +1,32 @@
 using UnityEngine;
+using TMPro;
 
 public class BasketScore : MonoBehaviour
 {
     public int score = 0;
     public string ballTag = "Basketball";
 
+    public TextMeshProUGUI scoreText;
+
+    private void Start()
+    {
+        UpdateScoreText();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(ballTag))
         {
             score++;
+            UpdateScoreText();
+        }
+    }
+
+    private void UpdateScoreText()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = score.ToString();
         }
     }
 }
